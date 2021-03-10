b13a0693e9707988eecc54f1da4868f9d3046310
Sam Niyazi <sam2003@niyazi.com>
2021-03-10 13:34:26 -0600

    Completed M05-05.  Incomplete implementation.

--- src/app/product/product-list.component.ts
+++ src/app/product/product-list.component.ts
@@ -1,9 +1,14 @@
 ﻿
 // 03/09/2021 04:10 pm - SSN - [20210309-1551] - [001] - M03-06 - Add product list component and HTML
+// 03/10/2021 01:29 pm - SSN - [20210310-1324] - [001] - M05-05 - Build product search class. Update product list component
+
 
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
 import { ProductService } from './product.service';
+import { CategoryService } from '../category/category.service';
+import { Category } from '../category/category';
+import { ProductSearch } from './productSearch';
 
 
 
@@ -14,8 +19,7 @@ import { ProductService } from './product.service';
 })
 export class ProductListComponent implements OnInit {
 
-
-    constructor(private productService: ProductService) {
+    constructor(private productService: ProductService, private categoryService: CategoryService) {
 
     }
 
@@ -28,6 +32,8 @@ export class ProductListComponent implements OnInit {
     products: Product[] = [];
     messages: string[] = [];
 
+    seachCategories: Category[] = [];
+    searchEntity: ProductSearch = new ProductSearch();
 
     private getProducts() {
 
---
+++ src/app/product/productSearch.ts
@@ -0,0 +1,7 @@
+﻿
+// 03/10/2021 01:29 pm - SSN - [20210310-1324] - [001] - M05-05 - Build product search class. Update product list component
+
+export class ProductSearch {
+    productName: string,
+    categoryId: number
+}
