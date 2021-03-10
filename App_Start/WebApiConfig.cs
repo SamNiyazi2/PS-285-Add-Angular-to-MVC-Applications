1592ef6bdd613426d2504288c40fd1724f8c6270
Sam Niyazi <sam2003@niyazi.com>
2021-03-09 18:09:45 -0600

    Completed M04-03.  Incomplete implementation.

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+
+// 03/09/2021 06:05 pm - SSN - [20210309-1802] - [001] - M04-03 - Add WebApiConfig class
+
+
+namespace PTC.App_Start
+{
+    public static class WebApiConfig
+    {
+
+        public static void Register(HttpConfiguration config)
+        {
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+
+        }
+
+    }
+}
