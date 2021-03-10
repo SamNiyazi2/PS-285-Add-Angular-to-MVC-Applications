14a88e4ceeba75343a5811d95f1e278d009b7505
Sam Niyazi <sam2003@niyazi.com>
2021-03-10 14:28:23 -0600

    Completed M05-07.  Incomplete implementation.

--- Controllers/ProductApiController.cs
+++ Controllers/ProductApiController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 
 
 // 03/09/2021 05:54 pm - SSN - [20210309-1750] - [001] - M04-02 - Add a Web API class
-
+// 03/10/2021 02:24 pm - SSN - [20210310-1420] - [001] - M05-07 - Create search Web AP
 
 namespace PTC.Controllers
 {
@@ -17,7 +17,7 @@ namespace PTC.Controllers
         public IHttpActionResult Get()
         {
 
-          //  throw new ApplicationException("mvc-err-ssn-20210308-1936: Test exception.");
+            //  throw new ApplicationException("mvc-err-ssn-20210308-1936: Test exception.");
 
             IHttpActionResult ret = null;
 
@@ -40,5 +40,32 @@ namespace PTC.Controllers
 
             return ret;
         }
+
+
+        [Route("Search")]
+        [HttpPost]
+        public IHttpActionResult Search([FromBody] ProductSearch search)
+        {
+
+            IHttpActionResult ret = null;
+
+            PTCViewModel vm = new PTCViewModel();
+
+            vm.SearchEntity = search;
+            vm.Search();
+
+            if (vm.LastException != null)
+            {
+                ret = BadRequest(vm.Message);
+            }
+            else
+            {
+                ret = Ok(vm.Products);
+            }
+
+            return ret;
+        }
+
+
     }
 }
