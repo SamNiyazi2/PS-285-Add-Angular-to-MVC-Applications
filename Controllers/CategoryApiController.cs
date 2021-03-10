2c136665a5035cbb4763c560c19a586ddd4dd7b8
Sam Niyazi <sam2003@niyazi.com>
2021-03-10 13:05:19 -0600

    Completed M05-03.  Incomplete implementation.

---
+++ Controllers/CategoryApiController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+// 03/10/2021 12:57 pm - SSN - [20210310-1254] - [001] - M05-03 - Build web API to get categories
+
+
+namespace PTC.Controllers
+{
+    [RoutePrefix("api/CategoryApi")]
+    public class CategoryApiController : ApiController
+    {
+        [HttpPost()]
+        [Route("SearchCategories")]
+        public IHttpActionResult GetSearchCategories()
+        {
+            IHttpActionResult ret = null;
+            PTCViewModel vm = new PTCViewModel();
+
+            vm.LoadSearchCategories();
+
+            if (vm.SearchCategories.Count() > 0)
+            {
+                ret = Ok(vm.SearchCategories);
+            }
+            else if (vm.LastException != null)
+            {
+                ret = BadRequest(vm.Message);
+            }
+            else
+            {
+                ret = NotFound();
+            }
+
+            return ret;
+        }
+
+    }
+}
