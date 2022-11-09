4c42956b3d94a5209175dad832a446f4a94eb616
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2022-11-09 12:43:42 -0600

    NuGet package ssn-angular-script-util-framework.

---
+++ dotnet_add_package_ssn-angular-util-framework_local.ps1
@@ -0,0 +1,28 @@
+# 110/09/2022 10:53 am - SSN - Copied from "C:\Sams_P\DevSitesIndex\DevSitesIndex\dotnet_add_package_DAL_local.ps1"
+
+$erroractionpreference = "stop"
+
+$error.clear()
+0..10|%{""}
+get-date 
+""
+
+
+write-host "testing-20221109-1152 " -foregroundcolor blue 
+
+. C:\Sams\PS\NuGet\add-nuget-package-util.ps1
+
+
+$projectName = "$psscriptroot\PTC.csproj"
+
+$packageName = "SSN_Angular_Script_Util_Framework"
+
+
+write-host "testing-20221109-1152-B " -foregroundcolor blue 
+
+ add-nugetPackage -projectName $projectName -packageName $packageName
+
+  
+
+   
+
